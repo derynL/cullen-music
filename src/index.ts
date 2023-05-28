f2e94519fa9f87d1285b0965b2025696d8b7e557
Deryn Cullen <deryn.cullen@sky.uk>
2023-05-28 15:44:02 +0100

    completed flowbite starter tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const text: string = 'Hello TypeScript';
-console.log(text);
+import { Modal } from 'flowbite';
+import type { ModalOptions, ModalInterface } from 'flowbite';
+
+const $buttonElement: HTMLElement = document.querySelector('#button');
+const $modalElement: HTMLElement = document.querySelector('#modal');
+const modalOptions: ModalOptions = {
+  placement: 'bottom-center',
+};
+const modal: ModalInterface = new Modal($modalElement, modalOptions);
+$buttonElement.addEventListener('click', () => modal.toggle());
+modal.show();
